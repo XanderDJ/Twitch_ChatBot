1359d394e2903eb23f771c63b676a15d01384229
xanderdj <x.de.j@hotmail.com>
2020-06-28 14:05:05 +0200

    Added missing file

---
+++ Chatbot.py
@@ -0,0 +1,22 @@
+from twitchchat import TwitchChat
+from credentials import *
+import sys
+import logging
+
+
+def new_message(msg):
+    print(msg)
+
+
+def new_subscriber(name, months):
+    print('New subscriber {0}! For {1} months'.format(name, months))
+
+
+args = sys.argv
+if len(args) < 2:
+    print("you need to add a streamer or multiple streamers in the commandline")
+else:
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+    tirc = TwitchChat(username, admin, oauth, [streamer for streamer in args[1:]])
+    tirc.start()
+    tirc.join()
