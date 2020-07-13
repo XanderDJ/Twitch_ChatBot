29c0e1d4f997b6083ce528ea52dec59bf47d704d
xanderdj <x.de.j@hotmail.com>
2020-07-13 14:26:31 +0200

    once more

--- mongoscripts/get_mentions.py
+++ mongoscripts/get_mentions.py
@@ -1,5 +1,5 @@
-from mongoscripts import client
-
+import pymongo
+client = pymongo.MongoClient("mongodb://localhost:27017/")
 twitch_db = client["twitch"]
 mentions = twitch_db["mentions"]
 for doc in mentions.find({}):
