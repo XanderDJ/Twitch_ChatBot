1ec3bcd70f28931ea8028edbb74ab62f7d05d133
xanderdj <x.de.j@hotmail.com>
2020-07-13 14:30:00 +0200

    wrong key

--- mongoscripts/get_mentions.py
+++ mongoscripts/get_mentions.py
@@ -3,7 +3,7 @@ client = pymongo.MongoClient("mongodb://localhost:27017/")
 twitch_db = client["twitch"]
 mentions = twitch_db["twitch"]
 for doc in mentions.find({}):
-    username = doc["username"]
+    username = doc["user"]
     message = doc["message"]
     timestamp = doc["timestamp"]
     print(timestamp)
