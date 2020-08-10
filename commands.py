3de7d7e899b9971d4070c97fd76214bb85ae7b01
xanderdj <x.de.j@hotmail.com>
2020-08-11 01:13:01 +0200

    fix replay

--- commands.py
+++ commands.py
@@ -755,7 +755,7 @@ def replay(bot: 'TwitchChat', args, msg, username, channel, send):
     msg = msg.lower()
     if contains_word(msg, ["!replay"]):
         if bot.limiter.can_send(channel, "replay", 60, True):
-            message = Message("Green OkayChamp", MessageType.COMMAND, channel)
+            message = Message("Not green FeelsWeirdMan", MessageType.COMMAND, channel)
             bot.send_message(message)
         return True
 
