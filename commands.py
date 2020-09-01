4549410c0cbe9308e2adc09f4c201d1623a5c575
xanderdj <x.de.j@hotmail.com>
2020-09-01 23:12:57 +0200

    changed rick roll vid

--- commands.py
+++ commands.py
@@ -503,7 +503,7 @@ def check_for_troll(bot: 'TwitchChat', channel: str, username: str, video_id: st
     try:
         title = response.get("items")[0].get("snippet").get("title")
         if contains_all(title.lower(), ["rick", "roll"]) or contains_all(title.lower(),
-                                                                         ["never", "gonna", "give", "you", "up", "rick",
+                                                                         ["gonna", "give", "you", "up", "rick",
                                                                           "astley"]):
             message = Message("Don't search that youtube video, " + username + " is trying to rick roll you!",
                               MessageType.HELPFUL, channel)
