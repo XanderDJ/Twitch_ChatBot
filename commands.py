a4cce430b8199e330d13a7fb60ba51266247a178
xanderdj <x.de.j@hotmail.com>
2020-09-08 19:16:46 +0200

    spammy command

--- commands.py
+++ commands.py
@@ -492,14 +492,6 @@ def remove_subscription(bot: 'TwitchChat', args, msg, username, channel, send):
 
 # COMMANDS
 
-@command
-@unwrap_command_args
-def auto_yuck(bot: 'TwitchChat', args, msg, username, channel, send):
-    if username.lower() == "autowalkmc":
-        message = Message(
-            "@autowalkmc, " + "PepeMods " * random.randint(1, 5) + " gross " + "PepeMods " * random.randint(1, 5),
-            MessageType.SPAM, channel)
-        bot.send_message(message)
 
 
 @command
