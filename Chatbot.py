ae7836d87bc489cb911d3babee14cec26a5a852f
xanderdj <x.de.j@hotmail.com>
2020-09-16 00:50:19 +0200

    removed unused methods

--- Chatbot.py
+++ Chatbot.py
@@ -3,15 +3,6 @@ from credentials.credentials import *
 import sys
 import logging
 
-
-def new_message(msg):
-    print(msg)
-
-
-def new_subscriber(name, months):
-    print('New subscriber {0}! For {1} months'.format(name, months))
-
-
 args = sys.argv
 if len(args) < 2:
     print("you need to add a streamer or multiple streamers in the commandline")
