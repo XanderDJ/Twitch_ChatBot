cb3f5f925d12d0a9068ac31096d054a2deff479a
xanderdj <x.de.j@hotmail.com>
2021-04-14 16:16:00 +0200

    fixed youtube link

--- commands.py
+++ commands.py
@@ -1763,7 +1763,8 @@ def check_youtube(bot: 'TwitchChat', args, msg, username, channel, send):
                 message = Message("@" + username + ", fuck off for trying that 4Weird", MessageType.COMMAND, channel,
                                   username)
             else:
-                message = Message("@" + username + " " + title + " - " + channel_name + " LINK: https://www.youtube.com/watch?v=" + video_id, MessageType.COMMAND, channel,
+                msg = title + " - " + channel_name + " - LINK: https://www.youtube.com/watch?v=" + video_id if "linker" in rbac.get_roles(username, channel) else title + " - " + channel_name
+                message = Message(msg, MessageType.COMMAND, channel,
                                   username)
         except IndexError as e:
             message = Message("@" + username + ", that youtube video doesn't exist", MessageType.COMMAND, channel,
