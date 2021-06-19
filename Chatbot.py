2d37dfc73c967b748a0e1c7129e7be1409af7cd1
xanderdj <x.de.j@hotmail.com>
2021-06-19 22:51:02 +0200

    trying to add reason for

--- Chatbot.py
+++ Chatbot.py
@@ -7,7 +7,7 @@ args = sys.argv
 if len(args) < 2:
     print("you need to add a streamer or multiple streamers in the commandline")
 else:
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
     twitch_irc = TwitchChat(username, admin, oauth, [streamer for streamer in args[1:]])
     twitch_irc.start()
     twitch_irc.join()
